59a03d52ba940d666646c4224fc2f7d193ee6085
Anand <anandmiskin221@gmail.com>
2024-11-01 18:36:50 +0530

    Header folder added

---
+++ score-board/src/Header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Score Board</h1>
+    </div>
+  );
+}
+
+export default Header;
